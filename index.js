ccc8c0b4a685b845e8eeb52b6025535cde02830b
UWINEZA7Gloria <gloria7wineza@gmail.com>
2024-02-23 08:32:10 -0800

    dogyear

---
+++ index.js
@@ -0,0 +1,12 @@
+const myAge=20;
+let earlyYears=2;
+earlyYears*=10.5;
+let laterYears=myAge-2;
+laterYears *=4;
+console.log(`earlyyearrs are ${earlyYears},lateryears are ${laterYears}`);
+//converting my age in dog's year
+var myAgeInDogYears=earlyYears+laterYears;
+//turning string in lowercase letters
+let myName="UWINEZA Gloria".toLowerCase();
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years`);
+
